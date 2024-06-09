d4651417162ef1a6de5bd64f6f030039d0691d9f
jakeschu <63879493+jakeschu@users.noreply.github.com>
2024-06-09 16:18:09 -0500

    Add files via upload

---
+++ CST-150 Milestone 1.cs
@@ -0,0 +1,23 @@
+namespace CST_150_Milestone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            string name = NameTextBox.Text;
+
+            string quantityString = QuantityTextBox.Text;
+            double quantity = double.Parse(quantityString);
+
+            string priceString = PriceTextBox.Text;
+            double price = double.Parse(priceString);
+
+            ListLabel.Text = quantity + " " + name + " for $" + Math.Round(price * quantity, 2);
+        }
+    }
+}
