fee982c15850fe7f058f24748f6c40ce44415c41
jakeschu <63879493+jakeschu@users.noreply.github.com>
2024-07-11 22:02:17 -0500

    CST-150 Activity 7 InvItem.cs

---
+++ CST-150 Activity 7 InvItem.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST_150_ListTogv.Models
+{
+    /// <summary>
+    /// Model Class that will structure all my inventory items. 
+    /// </summary>
+    public class InvItem
+    {
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public int Qty { get; set; }
+
+        /// <summary>
+        /// Model CLass Pararmterized Constructor.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="color"></param>
+        /// <param name="qty"></param>
+        public InvItem(string type, string color, int qty)
+        {
+            Type = type;
+            Color = color;
+            Qty = qty;
+        }
+    }
+}
